49929b24db7855e30bf9a2bdbb172de756d66ac4
Dante Nucci-Vladimir <dnuccivladimir@gmail.com>
2026-07-14 05:24:12 +0000

    Completed assignment 2 script.

--- assignment2.sh
+++ assignment2.sh
@@ -2,7 +2,7 @@
 
 echo ""
 
-# Checks to see if the script is ran as root.
+# Checks see if the script is ran as root.
 if [ "$EUID" -ne 0 ]; then
   echo "Please run this script as root."
   exit 1
